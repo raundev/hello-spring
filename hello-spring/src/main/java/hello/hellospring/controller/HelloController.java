58e38a65d1f0aa4e6537311606917face160e259
raundev <lbil7641@gmail.com>
2023-03-25 13:10:53 +0900

    김영한님 기초 강의 따라하기

---
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,48 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";    //viewResolver가 매핑
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody   //응답 body에 직접 data를 넣어주겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // JsonConverter / StringConverter가 처리
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
---
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,31 @@
+package hello.hellospring.domain;
+
+import jakarta.persistence.*;
+
+import javax.annotation.processing.Generated;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /*@Column(name="name")*/
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
